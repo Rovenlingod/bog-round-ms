a9c5a800d6b686e9a9e0bc1fe9878ae2bc9e3cdd
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-11-24 20:01:37 +0300

    Initial commit

---
+++ src/main/java/com/example/bogroundms/BogRoundMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bogroundms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogRoundMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogRoundMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bogroundms/BogRoundMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bogroundms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogRoundMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
