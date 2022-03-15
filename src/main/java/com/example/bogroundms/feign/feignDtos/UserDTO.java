9aa190132a96778005eadf6ef97fc113fd173768
Daniil Kovalyov <kovalyovd21@gmail.com>
2022-03-15 13:45:41 +0300

    - added security functionality - added auth token interceptor for feign requests - added validation exception handler

---
+++ src/main/java/com/example/bogroundms/feign/feignDtos/UserDTO.java
@@ -0,0 +1,18 @@
+package com.example.bogroundms.feign.feignDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String id;
+    private String login;
+    private String encryptedPassword;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
+
